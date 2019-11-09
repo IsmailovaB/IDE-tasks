a6ab2631997e681e66a8f65e99b43f02b5f42dae
IsmailovaB <56233988+IsmailovaB@users.noreply.github.com>
2019-11-09 17:00:05 -0600

    test

--- src/NewClass.java
+++ src/NewClass.java
@@ -2,11 +2,7 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Done");
-        for(;;){
-            System.out.println("Fine");
-            for(;;){
-
-
+      
                 System.out.println("flasdfgjladfhs");
             }
         }
